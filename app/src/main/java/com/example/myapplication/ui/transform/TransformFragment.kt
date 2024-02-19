9f952efa4fe168d80d2922b49dc2099be7cdcaf5
azar0420 <azar.17@osu.edu>
2024-02-18 19:43:15 -0500

    remove redundant icons

--- app/src/main/java/com/example/myapplication/ui/transform/TransformFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/transform/TransformFragment.kt
@@ -71,14 +71,6 @@ class TransformFragment : Fragment() {
                 R.drawable.avatar_6,
                 R.drawable.avatar_7,
                 R.drawable.avatar_8,
-                R.drawable.avatar_9,
-                R.drawable.avatar_10,
-                R.drawable.avatar_11,
-                R.drawable.avatar_12,
-                R.drawable.avatar_13,
-                R.drawable.avatar_14,
-                R.drawable.avatar_15,
-                R.drawable.avatar_16,
         )
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransformViewHolder {
